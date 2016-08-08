094b19b713151e7fb785ca2c04373aa0a48cc1ce
Luke Cahill <lmc@outlook.com>
2016-08-08 20:56:20 +0100

    Initial commit.

--- Email-Application/Form1.Designer.cs
+++ Email-Application/Form1.Designer.cs
@@ -23,12 +23,88 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.mailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.fetchNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.mailToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(675, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exitToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// newToolStripMenuItem
+			// 
+			this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+			this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.newToolStripMenuItem.Text = "&New";
+			this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.exitToolStripMenuItem.Text = "&Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// mailToolStripMenuItem
+			// 
+			this.mailToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fetchNewToolStripMenuItem});
+			this.mailToolStripMenuItem.Name = "mailToolStripMenuItem";
+			this.mailToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+			this.mailToolStripMenuItem.Text = "Mail";
+			// 
+			// fetchNewToolStripMenuItem
+			// 
+			this.fetchNewToolStripMenuItem.Name = "fetchNewToolStripMenuItem";
+			this.fetchNewToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.fetchNewToolStripMenuItem.Text = "&Fetch new";
+			this.fetchNewToolStripMenuItem.Click += new System.EventHandler(this.fetchNewToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(675, 296);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem mailToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem fetchNewToolStripMenuItem;
 	}
 }
 
--- Email-Application/Form1.cs
+++ Email-Application/Form1.cs
@@ -1,17 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using AE.Net.Mail;
+using System;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Email_Application {
 	public partial class Form1 : Form {
 		public Form1() {
 			InitializeComponent();
+		}
+
+		public void FetchMessages() {
+			using(var client = new ImapClient()) {
+				client.Connect("imap.gmail.com", 993, true, true);
+				var username = "";
+				var password = "";
+
+				client.Login(username, password);
+				GetMessages(client);
+			}
+		}
+
+		public void GetMessages(ImapClient client) {
+			if(client.IsConnected) {
+				client.SelectMailbox("inbox");
+				var messages = GetUnseenMessages(client);
+				var count = messages.Count();
+
+				if(count > 0) {
+					LoopThroughMessages(client, messages, count);
+				} else {
+					Debug.WriteLine("No new mail has been found.");
+				}
+			}
+		}
+
+		private Lazy<MailMessage>[] GetUnseenMessages(ImapClient client) {
+			var messages = client.SearchMessages(SearchCondition.Unseen());
+			return messages;
+		}
+
+		private void LoopThroughMessages(ImapClient client, Lazy<MailMessage>[] messages, int count) {
+			var j = 0;
+			MailMessage[] mailMessage = new MailMessage[count];
+			foreach (var item in messages) {
+				var message = item.Value.Body;
+				mailMessage[j] = item.Value;
+				MessageBox.Show(item.Value.Body);
+				j++;
+			}
+			client.SetFlags(Flags.Seen, mailMessage);
+			Debug.WriteLine("Mail has been fetched successfully!");
+		}
+
+		private void fetchNewToolStripMenuItem_Click(object sender, EventArgs e) {
+			FetchMessages();
+		}
+
+		private void newToolStripMenuItem_Click(object sender, EventArgs e) {
+			var info = new ProcessStartInfo(Application.ExecutablePath);
+			Process.Start(info);
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+			Environment.Exit(0);
 		}
 	}
 }
