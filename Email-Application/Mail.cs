72b74f227f6d2750c951a5536fc26246598f4dd6
Luke Cahill <lmc@outlook.com>
2016-08-08 21:21:31 +0100

    Add mail class

---
+++ Email-Application/Mail.cs
@@ -0,0 +1,13 @@
+﻿using AE.Net.Mail;
+using System.Net.Mail;
+using System.Collections.Generic;
+
+namespace Email_Application {
+	public class Mail {
+		public int MailId { get; set; }
+		public string MailBody { get; set; }
+		public string MailSubject { get; set; }
+		public ICollection<MailAddress> MailToAddress { get; set; }
+		public ICollection<MailAddress> MailCCAddresses { get; set; }
+	}
+}
