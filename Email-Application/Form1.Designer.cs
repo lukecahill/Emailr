e7f58e93c74b657856f86531560b399be3f4c8d5
Luke Cahill <lmc@outlook.com>
2016-08-08 21:22:41 +0100

    Add new items to the designer view.

--- Email-Application/Form1.Designer.cs
+++ Email-Application/Form1.Designer.cs
@@ -29,7 +29,13 @@
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.mailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.fetchNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.fetchNewMailButton = new System.Windows.Forms.Button();
 			this.menuStrip1.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
@@ -39,7 +45,7 @@
             this.mailToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(675, 24);
+			this.menuStrip1.Size = new System.Drawing.Size(688, 24);
 			this.menuStrip1.TabIndex = 0;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -81,17 +87,65 @@
 			this.fetchNewToolStripMenuItem.Text = "&Fetch new";
 			this.fetchNewToolStripMenuItem.Click += new System.EventHandler(this.fetchNewToolStripMenuItem_Click);
 			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.progressBar});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 558);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(688, 22);
+			this.statusStrip1.TabIndex = 2;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// progressBar
+			// 
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(150, 16);
+			this.progressBar.Step = 1;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(12, 115);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(665, 440);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(12, 77);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(665, 32);
+			this.richTextBox2.TabIndex = 4;
+			this.richTextBox2.Text = "";
+			// 
+			// fetchNewMailButton
+			// 
+			this.fetchNewMailButton.Location = new System.Drawing.Point(550, 27);
+			this.fetchNewMailButton.Name = "fetchNewMailButton";
+			this.fetchNewMailButton.Size = new System.Drawing.Size(126, 44);
+			this.fetchNewMailButton.TabIndex = 5;
+			this.fetchNewMailButton.Text = "Fetch New Mail";
+			this.fetchNewMailButton.UseVisualStyleBackColor = true;
+			this.fetchNewMailButton.Click += new System.EventHandler(this.fetchNewMailButton_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(675, 296);
+			this.ClientSize = new System.Drawing.Size(688, 580);
+			this.Controls.Add(this.fetchNewMailButton);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "Emailr";
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -105,6 +159,11 @@
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem mailToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem fetchNewToolStripMenuItem;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripProgressBar progressBar;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Button fetchNewMailButton;
 	}
 }
 
