7105ede03a97455dd4972c25bc65744d834397cf
Luke Cahill <lmc@outlook.com>
2016-08-10 22:08:15 +0100

    Edit mail class to enable sending of mail.

--- Email-Application/Mail.cs
+++ Email-Application/Mail.cs
@@ -1,12 +1,35 @@
-﻿using System.Net.Mail;
-using System.Collections.Generic;
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mail;
 
 namespace Email_Application {
 	public class Mail {
-		public int MailId { get; set; }
-		public string MailBody { get; set; }
-		public string MailSubject { get; set; }
-		public ICollection<MailAddress> MailToAddress { get; set; }
-		public ICollection<MailAddress> MailCCAddresses { get; set; }
+
+		public Mail() { }
+
+		public bool sendMail(string subject, string body, string to, string from) {
+			try {
+				var mail = new MailMessage();
+				var smtpServer = new SmtpClient("smtp.gmail.com");
+
+				mail.From = new MailAddress(from);
+				mail.To.Add(to);
+				mail.Subject = subject;
+				mail.Body = body;
+
+				smtpServer.Port = 587;
+				smtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+				smtpServer.EnableSsl = true;
+
+				smtpServer.Send(mail);
+				Debug.WriteLine("Mail sent");
+				return true;
+
+			} catch (Exception ex) {
+				Debug.WriteLine(ex.ToString());
+				return false;
+			}
+		}
+
 	}
 }
