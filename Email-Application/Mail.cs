16fcf2fe1301063607f153c24a9fcf9f2d94960a
Luke Cahill <lmc@outlook.com>
2016-08-14 16:10:37 +0100

    Sending mail uses the saved credentials to send.

--- Email-Application/Mail.cs
+++ Email-Application/Mail.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Diagnostics;
 using System.Net.Mail;
 
 namespace Email_Application {
 	public class Mail {
 
-		public Mail() { }
+		string username = "", password = "";
+
+		protected static IMongoClient _client;
+		protected static IMongoDatabase _database;
+
+		public Mail() {
+			_client = new MongoClient();
+			_database = _client.GetDatabase("test");
+		}
+
+		private async void GetCredentials() {
+			var collection = _database.GetCollection<BsonDocument>("emailcredentials");
+			var filter = new BsonDocument();
+			using (var cursor = await collection.FindAsync(filter)) {
+				while (await cursor.MoveNextAsync()) {
+					var batch = cursor.Current;
+					foreach (var document in batch) {
+						username = document["username"].ToString();
+						password = document["password"].ToString();
+					}
+				}
+			}
+		}
 
 		public bool SendMail(string subject, string body, string to, string from) {
 			try {
@@ -18,7 +42,7 @@ namespace Email_Application {
 				mail.Body = body;
 
 				smtpServer.Port = 587;
-				smtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+				smtpServer.Credentials = new System.Net.NetworkCredential(username, password);
 				smtpServer.EnableSsl = true;
 
 				smtpServer.Send(mail);
